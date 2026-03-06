c933aaf18b58ac6fbc58ea33e20a13890f886f54
Kartik Verma <kartikverma559@gmail.com>
2026-03-06 10:06:59 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (82.83%) - LeetSync

--- 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.java
+++ 1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.java
@@ -1,15 +1,6 @@
 class Solution {
     public boolean checkOnesSegment(String s) {
-        for(int i=s.length()-1;i>=0;i--)
-        {
-            if(s.charAt(i)=='1')
-            {
-                for(int j=i;j>=0;j--)
-                {
-                    if(s.charAt(j)=='0') return false;
-                }
-            }
-        }
+        if(s.contains("01")) return false;
         return true;
     }
 }
