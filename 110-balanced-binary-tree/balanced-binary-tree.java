acc6c5c7150cebe73ff62e5ba63b87c0ffe16d54
Kartik Verma <kartikverma559@gmail.com>
2026-03-23 20:55:26 +0530

    Time: 1 ms (25.92%) | Memory: 46.4 MB (8.71%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -1,41 +1,37 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public boolean isBalanced(TreeNode root) {
-        if(root==null) return true;
-        boolean left=isBalanced(root.left);
-        boolean right=isBalanced(root.right);
 
-        boolean diff=Math.abs(height(root.left)-height(root.right))<=1;
+class Pair {
+    boolean first;
+    int second;
 
-        if(left && right && diff)
-        {
-            return true;
-        }
-        else return false;
+    Pair(boolean first, int second) {
+        this.first = first;
+        this.second = second;
     }
-    int height(TreeNode root)
-    {
-        if(root==null) return 0;
+}
 
-        int left=height(root.left);
-        int right=height(root.right);
+class Solution {
 
-        int max=Math.max(left,right)+1;
+    public boolean isBalanced(TreeNode root) {
+        return isBalancedFast(root).first;
+    }
 
-        return max;
+    public Pair isBalancedFast(TreeNode root) {
+        if (root == null) {
+            return new Pair(true, 0);
+        }
+
+        Pair left = isBalancedFast(root.left);
+        Pair right = isBalancedFast(root.right);
+
+        boolean leftAns = left.first;
+        boolean rightAns = right.first;
+
+        boolean diff = Math.abs(left.second - right.second) <= 1;
+
+        int height = Math.max(left.second, right.second) + 1;
+
+        boolean isBalanced = leftAns && rightAns && diff;
+
+        return new Pair(isBalanced, height);
     }
 }
