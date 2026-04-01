b259c5c494b9b1650cc38082b48d80c0e7d30d7a
Kartik Verma <kartikverma559@gmail.com>
2026-04-01 10:34:47 +0530

    Time: 1 ms (1.29%) | Memory: 42.6 MB (98.75%) - LeetSync

---
+++ 2384-root-equals-sum-of-children/root-equals-sum-of-children.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean checkTree(TreeNode root) {
+        Queue<TreeNode> q=new LinkedList<>();
+
+        q.offer(root);
+        int sum=0;
+        while(!q.isEmpty())
+        {
+            TreeNode node=q.poll();
+
+            if(node.left!=null) sum+=node.left.val;
+            if(node.right!=null) sum+=node.right.val;
+        }
+        if(sum==root.val) return true;
+        else return false;
+    }
+}
