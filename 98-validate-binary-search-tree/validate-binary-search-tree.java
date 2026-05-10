ec358ea7b20e173527ee4490e4847a9ea9cf5173
Kartik Verma <kartikverma559@gmail.com>
2026-05-10 18:08:27 +0530

    Time: 2 ms (13.10%) | Memory: 45.1 MB (63.98%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -15,15 +15,39 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return helper(root,Long.MIN_VALUE,Long.MAX_VALUE);
+    //     return helper(root,Long.MIN_VALUE,Long.MAX_VALUE);
+    // }
+
+    // boolean helper(TreeNode root,long min,long max)
+    // {
+    //     if(root==null) return true;
+
+    //     if(root.val<=min || root.val>=max) return false;
+
+    //     return helper(root.left,min,root.val) && helper(root.right,root.val,max);
+
+    if(root==null) return true;
+
+    Stack<TreeNode> st=new Stack<>();
+    TreeNode pre=null;
+
+    while(root!=null || !st.isEmpty())
+    {
+        while(root!=null)
+        {
+            st.push(root);
+            root=root.left;
+        }
+
+        root=st.pop();
+        if(pre!=null && root.val<=pre.val) return false;
+
+        pre=root;
+        root=root.right;
     }
 
-    boolean helper(TreeNode root,long min,long max)
-    {
-        if(root==null) return true;
+    return true;
 
-        if(root.val<=min || root.val>=max) return false;
 
-        return helper(root.left,min,root.val) && helper(root.right,root.val,max);
     }
 }
