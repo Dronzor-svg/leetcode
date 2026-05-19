11c0eaa5a85ff45c90509b3f3118fcfd4057769c
Kartik Verma <kartikverma559@gmail.com>
2026-05-19 15:34:34 +0530

    Time: 7 ms (26.70%) | Memory: 80.5 MB (35.01%) - LeetSync

--- 2634-minimum-common-value/minimum-common-value.java
+++ 2634-minimum-common-value/minimum-common-value.java
@@ -1,34 +1,33 @@
-class Solution {
+public class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        // Set<Integer> st=new HashSet<>();
-        // for(int i=0;i<nums1.length;i++)
-        // {
-        //     st.add(nums1[i]);
-        // }
-
-        // for(int i=0;i<nums2.length;i++)
-        // {
-        //     if(st.contains(nums2[i])) return nums2[i];
-        // }
-
-
-        int i = 0;
-        int j = 0;
-        int len1 = nums1.length;
-        int len2 = nums2.length;
-        if (nums1[len1 - 1] < nums2[0] || nums2[len2 - 1] < nums1[0]) {
-            return -1;
+        
+        if (nums1.length > nums2.length) {
+            return getCommon(nums2, nums1);
         }
-        while (i < len1 && j < len2) {
-
-            if (nums1[i] == nums2[j]) {
-                return nums1[i];
-            } else if (nums1[i] < nums2[j]) {
-                i++;
-            } else {
-                j++;
+        
+        for (int num : nums1) {
+            if (binarySearch(num, nums2)) {
+                return num;
             }
         }
+
+        
         return -1;
+    }
+
+    private boolean binarySearch(int target, int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
     }
 }
