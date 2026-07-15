1f7e000890b51950a955f01fe869d2dddc6150a9
Kartik Verma <kartikverma559@gmail.com>
2026-07-15 12:52:40 +0530

    Time: 1 ms (83.11%) | Memory: 42.4 MB (74.78%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.java
@@ -1,26 +1,33 @@
+// class Solution {
+// //     public int gcdOfOddEvenSums(int n) {
+// //         int sumOdd=n*n;
+// //         int sumEven=n*(n+1);
+
+// //         int ans=gcd(sumOdd,sumEven);
+
+// //         return ans;
+// //     }
+    
+// //     int gcd(int sumOdd,int sumEven)
+// //     {
+// //         int ans=0;
+// //         int smaller=Math.min(sumOdd,sumEven);
+
+// //         for(int i=1;i<=smaller;i++)
+// //         {
+// //             if(sumOdd%i==0 && sumEven%i==0)
+// //             {
+// //                 ans=i;
+// //             }
+// //         }
+// //         return ans;
+// //     }
+        
+// // }
+
+
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        int sumOdd=n*n;
-        int sumEven=n*(n+1);
-
-        int ans=gcd(sumOdd,sumEven);
-
-        return ans;
+        return n;
     }
-    
-    int gcd(int sumOdd,int sumEven)
-    {
-        int ans=0;
-        int smaller=Math.min(sumOdd,sumEven);
-
-        for(int i=1;i<=smaller;i++)
-        {
-            if(sumOdd%i==0 && sumEven%i==0)
-            {
-                ans=i;
-            }
-        }
-        return ans;
-    }
-        
 }
